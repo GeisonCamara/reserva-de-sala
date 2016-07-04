e809482cc6c3f20c3fa422229aaafdd958067cfb
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-04 10:31:57 -0300

    Configurações do servidor

--- server/api/api.js
+++ server/api/api.js
@@ -1,14 +1,18 @@
 module.exports = function(server) {
-    // Create Express API's here
+    var express = require('express');
+    var Busca = require ('../routes/busca');
+    var Participante = require ('../routes/participante');
+    var Sala = require ('../routes/sala');
+    var Data = require ('../routes/data');
+    var Responsavel = require ('../routes/responsavel');
+    var Inserir = require ('../routes/inserir');
+    var Deletar = require ('../routes/deletar');
 
-    /*
-    server.post('/', function (req, res) {
-        var body = req.body;
-
-        res.status(200).json({
-            code: 200,
-            message: "success"
-        });
-    });
-    */
+    server.use('/busca', Busca);
+    server.use('/participante', Participante);
+    server.use('/sala', Sala);
+    server.use('/data', Data);
+    server.use('/responsavel', Responsavel);
+    server.use('/inserir', Inserir);
+    server.use('/deletar', Deletar);
 };
